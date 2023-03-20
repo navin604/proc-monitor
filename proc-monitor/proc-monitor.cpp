cc2a1e62b1c62d28b10881721da9672d058305af
navin604 <55507003+navin604@users.noreply.github.com>
2023-03-19 19:58:56 -0700

    Added: PPID and command retrieved for each PID

--- proc-monitor/proc-monitor.cpp
+++ proc-monitor/proc-monitor.cpp
@@ -19,6 +19,60 @@ void get_pid(std::vector<std::string> &pid_vec)
 
 }
 
+
+ std::string get_parent_id(std::string& pid)
+{
+    const auto path = "/proc/" + pid + "/status";
+    std::string ppid;
+    std::ifstream file(path);
+       
+    int count = 0;
+    while (std::getline(file, ppid)) 
+    {
+        ++count;
+         if (count == 7) 
+         {
+            break;
+         }
+    }
+
+    std::istringstream iss(ppid);
+    std::string ppid_val;
+    iss >> ppid_val >> ppid_val;
+    return ppid_val;
+
+}
+
+
+ std::string get_command(std::string& pid)
+{
+     const auto cmdline_path = "/proc/" + pid + "/cmdline";
+     std::string cmd;
+     std::ifstream file(cmdline_path);
+     std::getline(file, cmd);
+     return cmd;
+}
+
+
+
+void get_info(std::string& pid)
+{
+    
+    std::string ppid = get_parent_id(pid);
+    std::string cmd = get_command(pid);
+   
+    if (pid == "2065") {
+        std::cout << pid << "\n";
+        std::cout << ppid << "\n";
+        std::cout << cmd << "\n";
+    }
+    
+    
+
+
+}
+
+
 std::string get_num(std::string& entry)
 {
     // Split path using delimiter '/'
@@ -50,9 +104,15 @@ bool is_digit(std::string &pid)
 
 void start() 
 {
-	std::cout << "Start function\n";
 	std::vector<std::string> pid_vec;
 	get_pid(pid_vec);
+    std::cout << "| Parent ID  |   Process ID   |   Command Line   |\n";
+    std::cout << "-------------------------------------------------\n";
+    for (auto& pid : pid_vec)
+    {
+        get_info(pid);
+    }
+    
 
 }
 
--- proc-monitor/proc-monitor.h
+++ proc-monitor/proc-monitor.h
@@ -3,7 +3,15 @@
 
 #include "Core.h"
 
-void get_pid(std::vector<std::string> &pid_vec);
+void get_pid(std::vector<std::string>& pid_vec);
+void get_info(std::vector<std::string>& pid_vec);
+
+std::string get_parent_id(std::string& pid);
+
+std::string get_command(std::string& pid);
+
+
+
 std::string get_num(std::string &entry);
 bool is_digit(std::string& pid);
 void start();
