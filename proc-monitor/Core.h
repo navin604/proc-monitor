78d9b3aa4f06963a3fb6cc56a3f76f9d3afafe93
navin604 <55507003+navin604@users.noreply.github.com>
2023-03-20 22:11:45 -0700

    Added preprocessor include directives

--- proc-monitor/Core.h
+++ proc-monitor/Core.h
@@ -1,7 +1,18 @@
 #pragma once
+
 #include <vector>
 #include <string>
 #include <iostream>
 #include <cstdio>
 #include "proc-monitor.h"
 #include <fstream>
+#include <unistd.h>
+#include <sstream>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+// Included to ensure <arpa/inet.h> is compatible on multible systems
+#include <sys/types.h>
+#include <regex>
+#include <array>
+#include <cstring>
